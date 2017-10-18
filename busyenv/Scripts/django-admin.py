d9a392c3a06c5e97c3fe62c28c6443e8ad895637
Cherie Yang <cyang@common-lisp.net>
2017-10-18 03:42:46 -0400

    BLaarrghh

---
+++ busyenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\cheryl\master\code\wip projects\busybee\busyenv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()

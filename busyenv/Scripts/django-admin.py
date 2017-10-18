68510df2a4827790ec44c7fe2ef000a727587f65
Ryan Lam <darth.ryking@gmail.com>
2017-10-18 03:45:39 -0400

    Remove accidental venv

--- busyenv/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\users\cheryl\master\code\wip projects\busybee\busyenv\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
